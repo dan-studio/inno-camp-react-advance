3b2c8a0c7255ab9efc87e686f775550d356981ee
khjoo1203 <khjoo1203@naver.com>
2022-08-29 14:37:15 +0900

    module 수정

--- src/redux/module/musicSlice.js
+++ src/redux/module/musicSlice.js
@@ -1,22 +1,33 @@
-import {createSlice} from '@reduxjs/toolkit'
+import axios from "axios";
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+export const __getMusic = createAsyncThunk(
+  "music/GET_Music",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.get("http://localhost:3001/list");
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+export const __addMusic = createAsyncThunk(
+  "music/ADD_MUSIC",
+  async (payload, thunkAPI) => {
+    try {
+      const data = await axios.post("http://localhost:3001/list", payload);
+      return thunkAPI.fulfillWithValue(data.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+)
+
 
 const initialState = {
-  list:[
-    {
-      id:1,
-      artist: "Travis Scott",
-      title: "SICKO MODE",
-      coverUrl: "https://lh3.googleusercontent.com/PSIZ9cf9hpESZwcSz2ylS5I-zIREqCSagxV-X4CJqefrE0sRCktRtFw-a7PlkLygmg7k1nZREKCaSzY=w544-h544-l90-rj",
-      like: false,
-      comment:[
-        {
-          userName: "Sparta",
-          content: "리덕스!",
-          commentLike: false
-      }
-      ]
-    }
-  ]
+  list: [],
+  currentMusic: [],
 }
 
 const counterSlice = createSlice({
@@ -27,8 +38,21 @@ const counterSlice = createSlice({
       const new_music_list = [...state.list, action.payload]
       return {...state, list: new_music_list}
     }
+  },
+  extraReducers: {
+    [__getMusic.pending]: (state) => {
+      state.isLoading = true; //네트워크 요청이 시작되면 로딩상태를 true로 변경합니다.
+    },
+    [__getMusic.fulfilled]: (state, action) => {
+      state.isLoading = false; //네트워크 요청이 끝났으니, false로 변경합니다.
+      state.list = action.payload; //Store에 있는 list에 서버에서 가져온 music을 넣습니다.
+    },
+    [__getMusic.rejected]: (state, action) => {
+      state.isLoading = false; //에러가 발생했지만, 네트워크 요청이 끝났으니, false로 변경합니다.
+      state.err = action.payload; //catch된 error객체를 state.error에 넣음
+    }
   }
 })
 
-export const { addMusic } =counterSlice.actions
+export const { addMusic } = counterSlice.actions
 export default counterSlice.reducer
